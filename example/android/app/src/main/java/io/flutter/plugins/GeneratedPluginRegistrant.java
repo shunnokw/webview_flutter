d48cdc8c522ce36abc8b64d2f5533d12f0a8d1db
Jason Wong <nok10211021@gmail.com>
2019-12-21 22:08:56 +0800

    initial commit

---
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,27 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import dev.flutter.plugins.e2e.E2EPlugin;
+import io.flutter.plugins.webviewflutter.WebViewFlutterPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    E2EPlugin.registerWith(registry.registrarFor("dev.flutter.plugins.e2e.E2EPlugin"));
+    WebViewFlutterPlugin.registerWith(registry.registrarFor("io.flutter.plugins.webviewflutter.WebViewFlutterPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Volumes/Data/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jasonwong/.pub-cache/hosted/pub.dartlang.org/webview_flutter-0.3.18+1/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Volumes/Data/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.4"
+export "FLUTTER_BUILD_NUMBER=1.0.4"
